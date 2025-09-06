a63bc36b956c5bbc730f92ba4c544fca67dda4a3
BrokenByteOfCode <Andriy4521@ukr.net>
2025-09-06 13:46:39 +0300

    a

--- modubot/config.py
+++ modubot/config.py
@@ -8,11 +8,16 @@ API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 OWNER_NICKNAME = os.getenv('OWNER_NICKNAME', 'Nobody')
 
-SUDO_USERS_FILE = "SUDOUsers.json"
-RESTART_TIME_FILE = "restart_time.txt"
+DATA_FOLDER = Path("/app/data") if os.path.exists("/app/data") else Path(".")
+DATA_FOLDER.mkdir(exist_ok=True)
+
+SUDO_USERS_FILE = DATA_FOLDER / "SUDOUsers.json"
+RESTART_TIME_FILE = DATA_FOLDER / "restart_time.txt"
 LOCK_FILE = "modubot.lock"
-CONSOLE_LOGO_FILE = Path('ConsoleLogo.txt')
-MODULES_FOLDER = Path("ModuBotModules")
+CONSOLE_LOGO_FILE = Path("ConsoleLogo.txt")
+
+MODULES_FOLDER = DATA_FOLDER / "ModuBotModules"
+MODULES_FOLDER.mkdir(exist_ok=True)
 
 GITHUB_RAW_URL_APP = "https://raw.githubusercontent.com/BrokenByteOfCode/ModuBot/main/app.py"
 MODULES_REPO_URL = "https://api.github.com/repos/BrokenByteOfCode/ModuBotModules/contents"
